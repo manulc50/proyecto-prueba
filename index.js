2b3a8052665eb51fafe26f4d892b28fcf9841402
Manuel Lorenzo <manulc50@hotmail.com>
2023-09-05 05:46:01 +0200

    Agregado Menu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Hola Mundo");
 console.log("Hola Mundo");
 console.log("Buenas");
 console.log("Que Tal desde GitHub");
+console.log("Feature Menu");
 
 function hola() {
     return false;
