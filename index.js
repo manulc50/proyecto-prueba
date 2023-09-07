423d7aa5dcfea907ade874d147967524c51b0584
Manuel Lorenzo <manulc50@hotmail.com>
2023-09-07 05:24:06 +0200

    Prueba issue Fixes #6

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ console.log("Feature 2 Menu");
 console.log("Menu");
 console.log("Prueba cierre issue #4");
 console.log("Prueba cierre issue #5");
+console.log("Prueba cierre issue #6");
 
 function hola() {
     return false;
