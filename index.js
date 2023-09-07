7082df789dc0304780ef45ed618db3e233dbcbb3
Manuel Lorenzo <manulc50@hotmail.com>
2023-09-07 05:21:26 +0200

    Prueba issue Close #4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("Que Tal desde GitHub");
 console.log("Feature Menu");
 console.log("Feature 2 Menu");
 console.log("Menu");
-console.log("Prueba issue #4");
+console.log("Prueba cierre issue #4");
 
 function hola() {
     return false;
