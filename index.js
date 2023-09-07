7f49d300caa8b61ccf08bc3f4d828b4f91403e6b
Manuel Lorenzo <manulc50@hotmail.com>
2023-09-07 05:18:46 +0200

    Prueba issue #3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log("Que Tal desde GitHub");
 console.log("Feature Menu");
 console.log("Feature 2 Menu");
 console.log("Menu");
+console.log("Prueba cierre issue mediante commit");
 
 function hola() {
     return false;
